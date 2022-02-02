bd796f13bb0b51d7faacfbc51510e3bf97ae0526
IvanPyGit <93933140+IvanPyGit@users.noreply.github.com>
2022-02-02 14:34:21 +0300

    Добавлено приветствие

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
+import prompt
+
+
 def greet():
-    print("Welcome to the Brain Games!")
+    print('Welcome to the Brain Games!')
+    name = prompt.string("May I have your name? ")
+    print(f'Hello, {name}.')
+
 
 def main():
     greet()
+
 
 if __name__ == '__main__':
     main()
