f275bba148fdaeed32696eaa520d7bfb4f7e63a3
IvanPyGit <93933140+IvanPyGit@users.noreply.github.com>
2022-02-02 20:29:31 +0300

    Добавлено приветствие

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -5,7 +5,7 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def get_question_and_answer():
-    question = random.randint(1, 100)
+    question = random.randint(2, 100)
     for i in range(2, int(question / 2) + 1):
         if question % i == 0:
             answer = 'no'
