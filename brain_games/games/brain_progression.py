657db1c7d121aa2dde6b3a1c5a18fc290fa6a839
IvanPyGit <93933140+IvanPyGit@users.noreply.github.com>
2022-03-09 22:42:35 +0300

    Добавлено приветствие

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -4,6 +4,7 @@ import random
 DESCRIPTION = 'What number is missing in the progression?'
 LENGTH_PROGRESSION = 20 + 1
 
+
 def get_question_and_answer():
     start = random.randint(1, 30)
     step = random.randint(1, 10)
