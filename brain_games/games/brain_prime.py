ce3b147180c708b4c12af561abf021116c94d882
IvanPyGit <93933140+IvanPyGit@users.noreply.github.com>
2022-03-13 12:09:56 +0300

    Итоговый вариант

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -6,7 +6,7 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def get_question_and_answer():
     question = random.randint(2, 100)
-    answer = 'yes' if is_prime(question) is True else 'no'
+    answer = 'yes' if is_prime(question) else 'no'
     return answer, question
 
 
--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -9,9 +9,9 @@ def get_question_and_answer():
     start = random.randint(1, 30)
     step = random.randint(1, 10)
     stop = start + step * PROGRESSION_LENGTH
-    ap = list(range(start, stop, step))
+    progression = list(range(start, stop, step))
     hidden_element = random.randint(1, PROGRESSION_LENGTH - 1)
-    answer = str(ap[hidden_element])
-    ap[hidden_element] = '..'
-    question = ' '.join(map(str, ap))
+    answer = str(progression[hidden_element])
+    progression[hidden_element] = '..'
+    question = ' '.join(map(str, progression))
     return answer, question
